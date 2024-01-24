4dd33d20589483a800447615eb800fc8121a9203
piero.silvestri <piero.silvestri@overit.it>
2024-01-24 18:12:29 +0100

    Added shopping list and ingredients.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    RecipesComponent,
   ],
   imports: [
     BrowserModule
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recepi.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://toriavey.com/images/2011/01/TOA109_18-1-500x500.jpeg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://toriavey.com/images/2011/01/TOA109_18-1-500x500.jpeg')
+  ];
+
 
 }
